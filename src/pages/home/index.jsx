c332ca12f075e9662ea5e41353fca7e623065471
Yasin Altunisik <altun.yasin.isik@gmail.com>
2023-11-10 01:12:33 +0300

    sidebar added

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,3 +1,5 @@
+import MainLayout from "../../layouts/main";
+
 export default function Home(){
 return(
     <div>
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,23 +2,31 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/home";
 import Explore from "../pages/explore";
 import NotFound from "../pages/notFound";
+import Notifications from "../pages/notifications"
+import MainLayout from "../layouts/main";
 
 const routes = createBrowserRouter([
     {
         path: '/',
-        element: <Home/>
-    },
-    {
-        path: '/explore',
-        element: <Explore/>
-    },
-    {
-        path: '/notifications',
-        element: <Notification/>
-    },
-    {
-        path: '*',
-        element: <NotFound/>
+        element: <MainLayout/>,
+        children: [
+            {
+                index: true,
+                element: <Home/>,
+            },
+            {
+                path: 'explore',
+                element: <Explore/>
+            },
+            {
+                path: 'notifications',
+                element: <Notifications/>
+            },
+            {
+                path: '*',
+                element: <NotFound/>
+            }
+        ]
     }
 ])
 
