5c5a48f8ebd8a2d20a99e738bf4e64ab774bec9b
Yasin Altunisik <altun.yasin.isik@gmail.com>
2023-12-06 00:00:58 +0300

    test commit

--- src/layouts/main/sidebar/index.jsx
+++ src/layouts/main/sidebar/index.jsx
@@ -1,7 +1,7 @@
 export default function Sidebar(){
     return(
-        <aside className="w-[275px] min-h-screen">
-            sidebar
+        <aside className="w-[275px] min-h-screen ">
+            sidebarr
         </aside>
     )
 }
